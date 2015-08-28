054320fdb25b7e59433116029d74556ac6ddb067
MphoM <mpho112@gmail.com>
2015-08-28 18:51:56 +0200

    Fixed code

--- Scripts/Weather/New_Version/get_url_news8.py
+++ Scripts/Weather/New_Version/get_url_news8.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 __author__ = "Mpho Mphego"
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 __description__ = "News feeder retrieval"
-__date__ = "$Date: 2015/01/30 12:47$"
 __copyright__ = "Copyright (c) 2015 Mpho Mphego"
 __url__ = "mpho112.wordpress.com"
 __license__ = "Python"
 
 import feedparser
 
+# Today's news from News24.com
 try:
     rss = feedparser.parse('http://feeds.news24.com/articles/News24/SouthAfrica/rss')
-#    newsfeed = rss.entries[0]['title'] + '.  ' + rss.entries[0]['description'] + '.  ' + rss.entries[1]['title'] + '.  ' + rss.entries[1]['description'] + '.  ' + rss.entries[2]['title'] + '.  ' + rss.entries[2]['description'] + '.  ' + rss.entries[3]['title'] + '.  ' + rss.entries[3]['description'] + '.  '
-    newsfeed = rss.entries[0]['description'] + "  " + rss.entries[1]['description'] + "  " \
-               + rss.entries[2]['description'] + "  " + rss.entries[3]['description'] + "  " \
-               + rss.entries[4]['description'] + "  " + rss.entries[5]['description']
-#    print newsfeed
+    newsfeed = (rss.entries[0]['description'] + "\n" + rss.entries[1]['description']
+        + "\n" + rss.entries[2]['description'] + "\n" + rss.entries[3]['description']
+            + "\n" + rss.entries[4]['description'] + "\n" + rss.entries[5]['description'])
 
-# Today's news from News24.com
     news = ' And now, For the latest news around mzansi.  ' + newsfeed
 
 except rss.bozo:
     news = 'Failed to reach News24'
-
-print news
