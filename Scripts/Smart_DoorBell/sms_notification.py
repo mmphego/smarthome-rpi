d3538cd763c9732313c3d98cc0c195f9582add08
MphoM <mpho112@gmail.com>
2015-09-10 10:58:32 +0200

    added sms notification

---
+++ Scripts/Smart_DoorBell/sms_notification.py
@@ -0,0 +1,14 @@
+import urllib
+import time
+username = 'mpho112@gmail.com'
+password = '@Mpho100%'
+
+def sendSMS(uname, pword, numbers, sender, message):
+    params = {'uname': uname, 'pword': pword, 'selectednums': numbers,
+        'message' : message, 'from': sender}
+    f = urllib.urlopen('https://www.txtlocal.co.uk/sendsmspost.php?'
+        + urllib.urlencode(params))
+    return f.read()
+
+resp = sendSMS(username, password, '+27761431543',
+    'RPi', 'The was someone at the door on {}'.format(time.strftime("%c")))
