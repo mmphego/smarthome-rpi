f9457097b85a3000062912f96f7eeb236590b299
MphoM <mpho112@gmail.com>
2015-09-18 05:37:19 +0200

    Logging to CSV file

--- Scripts/Smart_DoorBell/WaitDoorbell.py
+++ Scripts/Smart_DoorBell/WaitDoorbell.py
@@ -10,24 +10,10 @@ __license__ = "Python"
 import RPi.GPIO as GPIO
 import time
 import os
-import logging
+from logger import LOGGER
 from push_notification import send_notification
 from sms_notification import send_sms
 from email_notification import send_mail
-
-# create logger
-LOGGER = logging.getLogger('Doorbell Logger')
-LOGGER.setLevel(logging.DEBUG) # log all escalated at and above DEBUG
-
-fh = logging.FileHandler('Doorbell_Logger.csv')
-fh.setLevel(logging.DEBUG) # ensure all messages are logged to file
-
-# create a formatter and set the formatter for the handler.
-frmt = logging.Formatter('%(asctime)s,%(name)s,%(levelname)s,%(message)s')
-fh.setFormatter(frmt)
-
-# add the Handler to the logger
-LOGGER.addHandler(fh)
 
 led = 17 #GPIO0
 button = 24 #GPIO1
