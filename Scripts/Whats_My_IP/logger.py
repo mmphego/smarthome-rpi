450877e6a0c6c2da7a80b5858fad913b69f71cb8
MphoM <mpho112@gmail.com>
2015-09-18 09:49:35 +0200

    Added CSV and logger

---
+++ Scripts/Whats_My_IP/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+"""
+Tracking events that happen when some software runs
+"""
+# create logger
+LOGGER = logging.getLogger('WhatsMy IP Logger')
+LOGGER.setLevel(logging.DEBUG) # log all escalated at and above DEBUG
+
+fh = logging.FileHandler('IP_Logger.csv')
+fh.setLevel(logging.DEBUG) # ensure all messages are logged to file
+
+# create a formatter and set the formatter for the handler.
+frmt = logging.Formatter('%(asctime)s,%(name)s,%(levelname)s,%(message)s')
+fh.setFormatter(frmt)
+
+# add the Handler to the logger
+LOGGER.addHandler(fh)
