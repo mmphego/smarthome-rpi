2ed4721083a1dd474f0282454db22ea1215eacbc
MphoM <mpho112@gmail.com>
2015-09-22 17:31:14 +0200

    Removed redundant script

--- Scripts/Website/DHT11.py
+++
@@ -1,7 +0,0 @@
-import time
-import Adafruit_DHT as dht
-
-with open('Temp_Humid.log','w') as fh:
-    fh.write(str(dht.read_retry(dht.DHT11, 26)))
-#varTemp, varHum = dht.read_retry(dht.DHT11, 26)
-#print varTemp, varHum
