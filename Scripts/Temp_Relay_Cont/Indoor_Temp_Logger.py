1b8029a948acb28c3b8eee38a3804b721eebe953
MphoM <mpho112@gmail.com>
2015-09-23 10:06:58 +0200

    writing to csv instead of txt

--- Scripts/Temp_Relay_Cont/Indoor_Temp_Logger.py
+++ Scripts/Temp_Relay_Cont/Indoor_Temp_Logger.py
@@ -1,9 +1,19 @@
+import csv
 import time
 import Adafruit_DHT as dht
 
-while True:
-    with open('/home/pi/Logs/Temp_Humid.log','w') as fh:
-        import IPython
-        varTemp, varHum = dht.read_retry(dht.DHT11, 26)
-        fh.write(list(dht.read_retry(dht.DHT11, 26)))
-    time.sleep(10)
+data = list(dht.read_retry(dht.DHT11, 26))
+path = "/home/pi/Logs/Temp_Humid.csv"
+
+#----------------------------------------------------------------------
+def csv_writer(data, path):
+    with open(path, "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(data)
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    while True:
+        csv_writer(data, path)
+        time.sleep(10)
