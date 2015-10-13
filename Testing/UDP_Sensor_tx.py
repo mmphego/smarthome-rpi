141ba386c49628b07194d07cdc470fa1b3207661
MphoM <mpho112@gmail.com>
2015-10-13 16:13:54 +0200

    Addede android helper

--- Testing/UDP_Sensor_tx.py
+++ Testing/UDP_Sensor_tx.py
@@ -2,44 +2,43 @@ try:
     # Android API Object SL4A Specific
     import android
 except:
-
     import androidhelper as android
 import time
 import socket
 
 droid = android.Android()
-hostname = "192.168.1.173"
-try:
-    if hostname > len(0):
-        pass
-except Exception as e:
-    droid.ttsSpeak(' data to host!')
-    hostname = str(droid.dialogGetInput().result)
 
-port = 5005
-data = ""
-ts = 100 # max sampling time 100ms
-tc = 1000 # cut of sampling time 1000ms
-t = 0
+try:
+    droid.ttsSpeak('Please enter server IP address')
+    time.sleep(.5)
+    # hostname = "192.168.1.173"
+    hostname = str(droid.dialogGetInput().result)
+    port = 5005
+    message = "Connecting to hostname:{}, port:{}".format(hostname, port)
+    droid.makeToast(hostname)
+except Exception as e:
+    droid.ttsSpeak('Failed to get IP address.')
+
+data = str()
+ts = 100  # max sampling time 100ms
 try:
 
     # Creating a UDP Socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Connecting to Host (Usually not required since it is UDP)
-    s.connect((hostname,port));
-    print "Connected!";
+    s.connect((hostname, port))
+    print "Connected!"
 except Exception as e:
     droid.ttsSpeak('Not connected to host!')
     print 'Failed: {}'.format(e)
 
-
-droid.startSensingTimed(2, ts) # start sensing accelerometer
+droid.startSensingTimed(2, ts)  # start sensing Accelerometer
 
 exit = False
 while not exit:
     sensor_data = str(droid.sensorsReadAccelerometer().result)
     try:
-        s.sendto(sensor_data,(hostname,port));
+        s.sendto(sensor_data, (hostname, port))
         time.sleep(.5)
     except Exception as e:
         print ' Could not connect to host, Error: {}'.format(e)
