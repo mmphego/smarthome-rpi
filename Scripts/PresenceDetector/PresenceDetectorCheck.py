b99da86580f63d3c5f1e2ce768dd980906b151a7
MphoM <mpho112@gmail.com>
2015-10-17 13:32:59 +0200

    Added new files

--- Scripts/PresenceDetector/PresenceDetectorCheck.py
+++ Scripts/PresenceDetector/PresenceDetectorCheck.py
@@ -11,17 +11,17 @@ import subprocess
 mac_address = 'bc:6e:64:df:d7:d9'
 mob_ip = None
 while True:
-    process = subprocess.Popen('sudo arp-scan --interface wlan0 -l | grep {} | cut -f 1'.format(mac_address),
-                               shell=True, stdout=subprocess.PIPE, )
+    process = subprocess.Popen(
+        'sudo arp-scan --interface wlan0 -l | grep {} | cut -f 1'
+            .format(mac_address), shell=True, stdout=subprocess.PIPE, )
     mob_ip = process.communicate()[0].strip()
     if mob_ip != '':
         break
 
 while True:  # Setup a while loop to wait for a button press
-    if subprocess.call(['ping -c1 {}'.format(mob_ip)], shell=True, stdout=open('/dev/null', 'w')) == 0:
+    if subprocess.call(['ping -c1 {}'.format(mob_ip)],
+        shell=True, stdout=open('/dev/null', 'w')) == 0:
         print "host is detected"
-
     else:
         print "host unreachable"
     break
-
