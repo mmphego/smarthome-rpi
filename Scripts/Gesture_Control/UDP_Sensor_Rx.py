3e5b665257a13e6a6d14c6ebb33ab88a57150046
MphoM <mpho112@gmail.com>
2015-12-16 13:07:58 +0200

    Gesture control revamped

--- Scripts/Gesture_Control/UDP_Sensor_Rx.py
+++ Scripts/Gesture_Control/UDP_Sensor_Rx.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 import socket
 import time
+import serial
 import numpy as np
 
 from logger import LOGGER
@@ -11,21 +12,37 @@ sock.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
 # Getting systems IP
 UDP_IP = sock.getsockname()[0]
 UDP_PORT = 5005
+nbytes = 1024
+time_out = 300
+sleep_time = 0.01
 
 # Range 30 - 60
-sensitivity = 25
+sensitivity = 30
 limit = 270
-# RC time constant
+# Low pass filter RC time constant
 alpha = 0.1
 x_data, y_data, z_data = [None] * 3
 
-LOGGER.info("Listening on IP:{}:{} ".format(UDP_IP, UDP_PORT))
-print("Listening on IP:{}:{} ".format(UDP_IP, UDP_PORT))
+baud = 9600
+arduino_output = {'relay1_on_off': 1, 'relay1_off': 10, 'relay1_on': 11,
+                  'relay2_off': 20, 'relay2_on': 21, 'relay3_off': 30,
+                  'relay3_on': 31, 'relay4_off': 40, 'relay4_on': 41,
+                  'relay5_off': 50, 'relay5_on': 51
+                  }
+locals().update(arduino_output)
+
+#try:
+    #serial_comm = serial.Serial('/dev/ttyACM0', baud)
+#except serial.SerialException:
+    #serial_comm = serial.Serial('/dev/ttyACM1', baud)
+
 try:
+    LOGGER.info("Listening on IP:{}:{} ".format(UDP_IP, UDP_PORT))
+    print("Listening on IP:{}:{} ".format(UDP_IP, UDP_PORT))
+
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
     sock.bind((UDP_IP, UDP_PORT))
-    sock.settimeout(30)
     LOGGER.info("Connected to host.")
     print("Connected to host.")
 except Exception as e:
@@ -35,9 +52,10 @@ except Exception as e:
 
 
 def gesture_control():
+    print 'Accelerometer: ',data
     x_data, y_data, z_data = eval(data)
     # Sleep for every samples.
-    time.sleep(1e-2)
+    time.sleep(sleep_time)
     Current_Acc = 0.0
     Prev_Acc = 0.0
     if x_data is not None:
@@ -50,46 +68,78 @@ def gesture_control():
         if Prev_Acc >= sensitivity <= limit:
             LOGGER.info('Mobile Shaken')
             print 'Mobile shaken'
+            #serial_comm.write(relay1_on_off)
 
 
-def voice_recognition():
+def voice_recognition(data):
     # TODO MM  2015/11/04
     # insert code here to switch on lights
-    if data == "kitchen light on" or data == "kitchen light off":
+
+    if data == "bedroom light on" or data == "bedroom on":
+        #serial_comm.write(relay1_on)
+        print data
+        LOGGER.info('Data: {}'.format(data))
+
+    elif data == "bedroom light off" or data == "bedroom off":
+        #serial_comm.write(relay1_off)
+        print data
+        LOGGER.info('Data: {}'.format(data))
+
+    elif data == "kitchen light on" or data == "kitchen on":
+        #serial_comm.write(relay2_on)
+        print data
+        LOGGER.info('Data: {}'.format(data))
+
+    elif data == "kitchen light off" or data == "kitchen off":
+        #serial_comm.write(relay2_off)
         print data
         LOGGER.info('Data: {}'.format(data))
 
     elif data == "dining light on" or data == "dining light off":
+        #serial_comm.write(relay3_on)
         print data
         LOGGER.info('Data: {}'.format(data))
 
-    elif data == "bedroom light on" or data == "bedroom light off":
+    elif data == "dining light off" or data == "dining off":
+        #serial_comm.write(relay3_off)
         print data
         LOGGER.info('Data: {}'.format(data))
 
     elif data == "tv room light on" or data == "tv room light off":
+        #serial_comm.write(relay4_on)
         print data
         LOGGER.info('Data: {}'.format(data))
 
-    elif data == "all lights on" or data == "all lights on":
+    elif data == "tv room light off" or data == "tv room off":
+        #serial_comm.write(relay4_off)
+        print data
+        LOGGER.info('Data: {}'.format(data))
+
+    elif data == "all lights on" or data == "lights on":
+        #serial_comm.write(relay5_on)
+        print data
+        LOGGER.info('Data: {}'.format(data))
+
+    elif data == "all lights off" or data == "lights off":
+        #serial_comm.write(relay5_off)
         print data
         LOGGER.info('Data: {}'.format(data))
 
     else:
-        print "invalid parameter"
+        print "invalid parameter: ", data
 
 
 while True:
     # TODO MM 2015/11/04
     # Combine gesture control and voice recognition
     # buffer size is 1024 bytes
-    data, addr = sock.recvfrom(1024)
+    data, addr = sock.recvfrom(nbytes)
     # format
     # data : str containing list
     # addr : 'xxx.xxx.xxx.xxx'
-    print len(data)
-    if len(data) > 50:
+    # print 'data length', len(data)
+    try:
+        eval(data)
         gesture_control()
-    else:
-        print data
-        voice_recognition()
+    except Exception:
+        voice_recognition(data)
--- Scripts/Gesture_Control/UDP_Sensor_Tx.py
+++ Scripts/Gesture_Control/UDP_Sensor_Tx.py
@@ -8,52 +8,116 @@ import socket
 
 droid = android.Android()
 
-try:
-    droid.ttsSpeak("Please enter server's IP address")
-    time.sleep(.5)
-    hostname = "192.168.1.173"
-    # TODO save IP to file and compare, if changed request user to input IP
-    with open('/sdcard/com.hipipal.qpyplus/scripts/ip.txt', 'w') as f:
-        Prev_IP = str(droid.dialogGetInput(title='IP Address').result)
-        f.write(Prev_IP)
-
-    with open('/sdcard/com.hipipal.qpyplus/scripts/ip.txt', 'r') as f:
-
-
-
-
-    port = 5005
-    message = "Connecting to hostname:{}, port:{}".format(hostname, port)
-    droid.makeToast(hostname)
-except Exception as e:
-    droid.ttsSpeak('Failed to get IP address.')
-
+options = ["Gesture Control", "Voice Recognition"]
 data = str()
 ts = 100  # max sampling time 100ms
-timeout = 0.5
+time_out = 0.5
 Accelerometer = 2
+port = 5005
+nbytes = 1024
+
+def radioButtonDialog(droid, question, options):
+    droid.dialogCreateAlert(question)
+    droid.dialogSetSingleChoiceItems(options)
+    droid.dialogSetPositiveButtonText("OK")
+    droid.dialogSetNegativeButtonText("Cancel")
+    droid.dialogShow()
+    response = droid.dialogGetResponse().result
+    if "which" in response:
+        result = response["which"]
+    if result == "negative":
+        raise Exception("Aborted")
+    selectedItems = droid.dialogGetSelectedItems().result
+    droid.dialogDismiss()
+    return selectedItems.pop()
+
 try:
-    # Creating a UDP Socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Connecting to Host (Usually not required since it is UDP)
-    s.connect((hostname, port))
-    droid.makeToast("Connected to server!")
-except Exception as e:
-    droid.ttsSpeak('Not connected to host due to {}!'.format(e))
-    droid.makeToast('Failed: {}'.format(e))
+    result = radioButtonDialog(droid, "Choose a gesture.", options)
+except Exception:
+    message = "Exiting application."
 
-# start sensing Accelerometer at 100ms
-droid.startSensingTimed(Accelerometer, ts)
-exit = False
-while not exit:
-    sensor_data = str(droid.sensorsReadAccelerometer().result)
+if options[result] == options[0]:
+
     try:
-        time.sleep(timeout)
-        s.sendto(sensor_data, (hostname, port))
-        s.settimeout(timeout)
+        droid.ttsSpeak("Please enter server's IP address")
+        time.sleep(time_out)
+        #hostname = "192.168.1.173"
+        # TODO save IP to file and compare, if changed request user to input IP
+        #with open('/sdcard/com.hipipal.qpyplus/scripts/ip.txt', 'w') as f:
+            #Prev_IP = str(droid.dialogGetInput(title='IP Address').result)
+            #f.write(Prev_IP)
+        hostname = str(droid.dialogGetInput(title='IP Address').result)
+        message = "Connecting to hostname:{}, port:{}".format(hostname, port)
+        droid.makeToast(hostname)
     except Exception as e:
-        droid.makeToast(' Could not connect to host, Error: {}'.format(e))
-        droid.ttsSpeak('Failed to send data to host!')
-        droid.stopSensing()
-        exit = True
+        droid.ttsSpeak('Failed to get IP address.')
 
+
+    try:
+        # Creating a UDP Socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Connecting to Host (Usually not required since it is UDP)
+        s.connect((hostname, port))
+        droid.makeToast("Connected to server!")
+    except Exception as e:
+        droid.ttsSpeak('Not connected to host due to {}!'.format(e))
+        droid.makeToast('Failed: {}'.format(e))
+
+    # start sensing Accelerometer at 100ms
+    droid.startSensingTimed(Accelerometer, ts)
+    exit = False
+    while not exit:
+        sensor_data = str(droid.sensorsReadAccelerometer().result)
+        try:
+            time.sleep(time_out)
+            s.sendto(sensor_data, (hostname, port))
+            s.settimeout(time_out)
+        except Exception as e:
+            droid.makeToast(' Could not connect to host, Error: {}'.format(e))
+            droid.ttsSpeak('Failed to send data to host!')
+            droid.stopSensing()
+            exit = True
+
+elif options[result] == options[1]:
+
+    try:
+        # TODO Add an IP checker, If it has changed ttsSpeak else use one from file
+       # droid.ttsSpeak("Please enter server's IP address")
+        time.sleep(time_out)
+        hostname = "192.168.0.103"
+        #hostname = str(droid.dialogGetInput(title='IP Address').result)
+        message = "Connecting to hostname:{}, port:{}".format(hostname, port)
+        droid.makeToast(hostname)
+    except Exception as e:
+        droid.ttsSpeak('Failed to get IP address.')
+
+    try:
+        # Creating a UDP Socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Connecting to Host (Usually not required since it is UDP)
+        s.connect((hostname, port))
+        droid.makeToast("Connected to server!")
+    except Exception as e:
+        droid.ttsSpeak('Not connected to host due to {}'.format(e))
+        droid.makeToast('Failed: {}'.format(e))
+
+    while True:
+        # Using Android API for Google Voice and Recognizing command
+        if data == '':
+            droid.ttsSpeak('Speak now')
+        data = droid.recognizeSpeech('Speak now', None, None)
+        if data.error != None:
+            pass
+        else:
+            data = str(data.result)
+            droid.makeToast(data)
+            if data == 'exit':
+                droid.ttsSpeak('Exiting program, goodbye!')
+                break
+            else:
+                s.sendto(data, (hostname, port))
+                droid.ttsSpeak(data)
+
+else:
+    droid.makeToast("Aborted")
+    raise Exception("Aborted")
